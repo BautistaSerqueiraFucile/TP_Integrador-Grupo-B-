dc50cd0ee5f05f661779fcf5b9e069267bc78d1b
Bautista Serqueira Fucile <id640794@outlook.com>
2025-06-09 20:41:37 -0300

    estructura principal de los microservicios realizada

---
+++ msvc-admin/src/main/java/org/example/msvcadmin/MsvcAdminApplication.java
@@ -0,0 +1,13 @@
+package org.example.msvcadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsvcAdminApplication.class, args);
+    }
+
+}
