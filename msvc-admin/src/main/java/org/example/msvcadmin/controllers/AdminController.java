bce939eda21c6f0d8095a6cce0f648e96141eba4
GermanSilva88 <130848764+GermanSilva88@users.noreply.github.com>
2025-06-14 14:44:54 -0300

    Comenzando con adminController

--- msvc-admin/src/main/java/org/example/msvcadmin/controllers/AdminController.java
+++ msvc-admin/src/main/java/org/example/msvcadmin/controllers/AdminController.java
@@ -1,4 +1,32 @@
 package org.example.msvcadmin.controllers;
 
+import org.example.msvcadmin.services.AdminService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin")
+
 public class AdminController {
+
+    private final AdminService adminService;
+
+    public AdminController(AdminService adminService){
+        this.adminService = adminService;
+    }
+
+
+    @PutMapping("/cuentas/{cuentaId}/bloquear")
+    public ResponseEntity<String> bloquearCuenta(@PathVariable Long cuentaId) {
+        boolean resultado = adminService.bloquearCuenta(cuentaId);
+        if (resultado) {
+            return ResponseEntity.ok("Cuenta bloqueada exitosamente.");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_GATEWAY)
+                    .body("No se pudo bloquear la cuenta.");
+        }
+    }
+
+
+
 }
