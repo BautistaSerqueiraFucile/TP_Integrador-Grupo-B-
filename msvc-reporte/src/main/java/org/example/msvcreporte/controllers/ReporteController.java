a3288b148dbd2cee1e3039c6b53a114522c0603f
unknown <figuta@gmail.com>
2025-06-14 15:49:41 -0300

    Agregado msvc reporte

--- msvc-reporte/src/main/java/org/example/msvcreporte/controllers/ReporteController.java
+++ msvc-reporte/src/main/java/org/example/msvcreporte/controllers/ReporteController.java
@@ -1,4 +1,24 @@
 package org.example.msvcreporte.controllers;
 
+import org.example.msvcreporte.services.ReporteService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reportes")
+
 public class ReporteController {
+    private final ReporteService reporteService;
+
+    public ReporteController(ReporteService reporteService) {
+        this.reporteService = reporteService;
+    }
+
+    @GetMapping(/usuarios/{id})
+    Public Usuario getUsuarioId(id){
+        return reporteService.getUsuarioId(id);
+    }
+
+
 }
