5b41cad037bf06bf12c6adf29b4d0f0765051e2d
Luciana La Battaglia <lucianalabattaglia@gmail.com>
2025-07-08 18:30:41 -0300

    modifica config por documentacion, agrega doc monop y parada

--- msvc-monopatin/src/main/java/org/example/msvcmonopatin/DTO/MonopatinEstadisticasDTO.java
+++ msvc-monopatin/src/main/java/org/example/msvcmonopatin/DTO/MonopatinEstadisticasDTO.java
@@ -1,14 +1,35 @@
 package org.example.msvcmonopatin.DTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Schema(description = "DTO con estadísticas de uso del monopatín")
 public class MonopatinEstadisticasDTO {
-    private String idMonopatin;
-    private double kilometros;
-    private double tiempoTotal;
-    private double tiempoPausa;
 
+    @Schema(
+            description = "ID único del monopatín",
+            example = "mnp-001"
+    )
+    private String idMonopatin;
+
+    @Schema(
+            description = "Distancia total recorrida por el monopatín (en kilómetros)",
+            example = "254.75"
+    )
+    private double kilometros;
+
+    @Schema(
+            description = "Tiempo total de uso del monopatín (en horas)",
+            example = "35.2"
+    )
+    private double tiempoTotal;
+
+    @Schema(
+            description = "Tiempo total en pausa (en horas)",
+            example = "4.75"
+    )
+    private double tiempoPausa;
 }
